82c954204955be04f3bd0d5d51f69a39ac5d1fed
bagjinhwan <spdlqjxp@gmail.com>
2024-04-27 01:16:22 +0900

    [Fix] 240427_Fix signup service to make entity with builder

--- backend/web/src/main/java/org/web/service/SignupService.java
+++ backend/web/src/main/java/org/web/service/SignupService.java
@@ -44,16 +44,35 @@ public class SignupService {
         } else {
             System.out.println("비밀번호가 옳지 않습니다.");
         }
+//        if (userRepository.existsByUserId(userId)) {
+//            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+//        }
+//
+//        if (userRepository.existsByEmail(email)) {
+//            throw new IllegalArgumentException("이미 존재하는 이메일 주소입니다.");
+//        } 이렇게 확인하는 방법은 어때
 
-        UserEntity data = new UserEntity();
+//        UserEntity data = new UserEntity();
+//
+//        data.setUserId(userId);
+//        data.setUserName(userName);
+//        data.setEmail(email);
+//        data.setPassword(bCryptPasswordEncoder.encode(password));
+//        data.setPasswordCheck(bCryptPasswordEncoder.encode(passwordCheck));
+//        data.setPhone(phone);
+//        data.setBirth(birth);
+//        data.setProvider("basic");
 
-        data.setUserId(userId);
-        data.setUserName(userName);
-        data.setEmail(email);
-        data.setPassword(bCryptPasswordEncoder.encode(password));
-        data.setPasswordCheck(bCryptPasswordEncoder.encode(passwordCheck));
-        data.setPhone(phone);
-        data.setBirth(birth);
+        //이런 방법도 있음
+        UserEntity data = UserEntity.builder()
+                .userId(userId)
+                .userName(userName)
+                .email(email)
+                .password(bCryptPasswordEncoder.encode(password))
+                .phone(phone)
+                .birth(birth)
+                .provider("basic")
+                .build();
 
         userRepository.save(data);
 
