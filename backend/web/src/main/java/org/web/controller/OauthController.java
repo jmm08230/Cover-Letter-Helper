d14f5489f693832125a0e93cbd2afb43f8220219
bagjinhwan <spdlqjxp@gmail.com>
2024-04-27 01:19:16 +0900

    [FEAT] 240427_Add new Oauth controller

---
+++ backend/web/src/main/java/org/web/controller/OauthController.java
@@ -0,0 +1,22 @@
+package org.web.Controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/oauth")
+public class OauthController {
+    @GetMapping("/loginfo")
+    public String getJson(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        // OAuth2User에서 사용자의 속성을 가져옴
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        // 속성을 문자열로 반환
+        return attributes.toString();
+    }
+}
