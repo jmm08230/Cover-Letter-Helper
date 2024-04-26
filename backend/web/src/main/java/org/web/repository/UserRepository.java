8216808242f193cd41d165aaed6ea6c3592a2e84
bagjinhwan <spdlqjxp@gmail.com>
2024-04-27 01:25:41 +0900

    [FEAT] 240427_add Method for login & Oauth

--- backend/web/src/main/java/org/web/repository/UserRepository.java
+++ backend/web/src/main/java/org/web/repository/UserRepository.java
@@ -3,10 +3,12 @@ package org.web.repository;
 import org.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     Boolean existsByUserId(String userId);
     Boolean existsByEmail(String email);
-
-
+    UserEntity findByUserId(String userId);
+    Optional<UserEntity> findUserByEmailAndProvider(String email, String provider);
 }
