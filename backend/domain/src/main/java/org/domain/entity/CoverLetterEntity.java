c733697e42aca92ced59ae82b86c0e6480288426
bagjinhwan <spdlqjxp@gmail.com>
2024-05-13 14:49:11 +0900

    [FEAT] 240513_Add CoverLetterEntity

---
+++ backend/domain/src/main/java/org/domain/entity/CoverLetterEntity.java
@@ -0,0 +1,32 @@
+package org.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CoverLetterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column
+    private String userId;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public void patch(CoverLetterEntity coverLetter) {
+        if (coverLetter.title != null) {
+            this.title = coverLetter.title;
+        }
+        if (coverLetter.content != null) {
+            this.content = coverLetter.content;
+        }
+    }
+}
